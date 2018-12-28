02308614bca098062e1db82c884a45397b52f381
Zeki Esenalp <160202033@kocaeli.edu.tr>
2018-12-28 17:44:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Yuv2Bmp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(538, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dosya Aç";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(183, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 30);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Height";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "420",
+            "422",
+            "444"});
+            this.comboBox1.Location = new System.Drawing.Point(365, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(1217, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 56);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Tekrar Başlat";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(294, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(720, 351);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(721, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(204, 39);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(1027, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 56);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Oynat";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.Location = new System.Drawing.Point(1440, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "İşlem";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1799, 1055);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yuv2Bmp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Yuv2Bmp
+{
+
+    /*
+     Ahmet Manga - 160202008
+     Zeki Esenalp - 160202033
+         */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string folder;
+        string file_path;
+        int width, height;
+        byte[] ms;
+        int file_type;
+        byte[] y, u, v;
+        int bmp_isim = 1;
+        int oynatilan = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || !IsNumeric(textBox1.Text) || !IsNumeric(textBox2.Text))
+            {
+                MessageBox.Show("Tüm alanları doldurun.");
+            }
+            else
+            {
+               
+                file_type = int.Parse(comboBox1.SelectedItem.ToString());
+                openFileDialog1.Title = "Yuv Dosyası Seçin";
+                openFileDialog1.Filter = "yuv files (*.yuv) |*.yuv";
+                openFileDialog1.FileName = "";
+                label3.Text = "Devam ediyor.";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    button1.Enabled = false;
+                    file_path = openFileDialog1.FileName;
+                    width = int.Parse(textBox1.Text);
+                    height = int.Parse(textBox2.Text);
+                    StreamReader s = new StreamReader(file_path);
+                    Stream stream = File.OpenRead(file_path);
+                    ms = ReadFile(stream);
+                    // 444 için
+                    
+                    if (file_type == 444)
+                    {
+                        int length = ms.Length;
+                        int frameSize = length / (width * height);
+                        
+                        for (int f = 0; f < frameSize; f++)
+                        {
+                            if (f % 3 == 0)
+                            {
+                                y = new byte[width * height];
+                                u = new byte[width * height];
+                                v = new byte[width * height];
+                            }
+                            int sira = 0;
+                            for (int l = f * (width * height); l < (f + 1) * (width * height); l++)
+                            {
+                                if (f % 3 == 0)
+                                {
+                                    y[sira] = ms[l];
+                                }
+                                if (f % 3 == 1)
+                                {
+                                    u[sira] = ms[l];
+                                }
+                                else
+                                {
+                                    v[sira] = ms[l];
+                                }
+                                sira++;
+                            }
+
+                            if (f % 3 == 2)
+                            {
+                                YuvToRgb(y, u, v, bmp_isim.ToString());
+                                bmp_isim++;
+
+                            }
+                        }
+                    }
+                    else if (file_type == 422)
+                    {
+                        int length = ms.Length;
+                        int frameSize = length / (width * height);
+                        
+                        for (int f = 0; f < frameSize; f++)
+                        {
+                            if (f % 2 == 0)
+                            {
+                                y = new byte[width * height];
+                                u = new byte[(width * height)];
+                                v = new byte[(width * height)];
+                            }
+                            int sira = 0;
+                            bool gecis = false;
+                            for (int l = f * (width * height); l < (f + 1) * (width * height); l++)
+                            {
+                                if (f % 2 == 0)
+                                {
+                                    y[sira] = ms[l];
+                                }
+                                else
+                                {
+                                    if (((f+1)*width*height - l) < (width*height)/2 )
+                                    {
+                                       
+                                        if (!gecis)
+                                        {
+                                            sira = 0;
+                                            gecis = true;
+                                        }
+                                        v[sira] = ms[l];  
+                                    }
+                                    else
+                                    {
+                                  
+                                      
+                                        u[sira] = ms[l];
+                                    }
+                                }
+                                sira++;
+                            }
+
+                            if (f % 2 == 1)
+                            {
+                                YuvToRgb(y, u, v, bmp_isim.ToString());
+                                bmp_isim++;
+
+                            }
+                        }
+
+                    }
+                    else if (file_type == 420)
+                    {
+                        int length = ms.Length;
+                        double frameSize2 = length / ((width * height)*(1.5)); int frameSize = Convert.ToInt32(frameSize2);
+
+                        for(int f = 0; f < frameSize; f++)
+                        {
+                            y = new byte[width * height];
+                            u = new byte[(width * height)];
+                            v = new byte[(width * height)];
+                            int sira = 0;
+                            double l = 0;
+                            for (l = f * ((width * height)*1.5); l < (width * height) + ((width * height) * f * 1.5); l++)
+                            {
+                                int l2 = Convert.ToInt32(l);
+                                y[sira] = ms[l2];
+                                sira++;
+                            }
+                          //  MessageBox.Show(" f : " + f + " - sira : " + sira.ToString() + " l : " + l);
+                            double l_u = 0;
+                            sira = 0; 
+                           for(l_u = l; l_u < ((width * height) + ((width*height)/4)) + ((width * height) * f); l_u++)
+                            {
+                                int l_u2 = Convert.ToInt32(l_u);
+                                u[sira] = ms[l_u2];
+                                sira++;
+                            }
+                            double l_v = 0;
+                            sira = 0;
+                            for (l_v = l_u; l_v < (f + 1) * ((width * height) * (1.5)); l_v++)
+                            {
+                                int l_v2 = Convert.ToInt32(l_v);
+                                v[sira] = ms[l_v2];
+                                sira++;
+                            }
+                            // MessageBox.Show("f: " + f + " y- " + y[0] + " u- " + u[0] + " v " + v[0] + " ---- l_v " + l_v );
+                            YuvToRgb(y, u, v, bmp_isim.ToString());
+                            bmp_isim++;
+                        }
+                      
+                    }
+                    label3.Text = "Tamamlandı";
+                    label3.ForeColor = Color.Green;
+                    System.Diagnostics.Process.Start(folder);
+                    if (comboBox2.Items[0].ToString() == "1")
+                    {
+                        pictureBox1.Image = Image.FromFile(folder + "\\1.bmp");
+                    }
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+        bool IsNumeric(string text)
+        {
+            foreach (char chr in text)
+            {
+                if (!Char.IsNumber(chr)) return false;
+            }
+            return true;
+        }
+        public void YuvToRgb(byte[] y, byte[] u, byte[] v, string isim)
+        {
+            byte[] rgb = new byte[width * height * 4];
+            Bitmap bm = new Bitmap(width, height);
+
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int i = h * width + w;
+                   
+                    double r = (y[i] + 1.4075);
+                    double g = (y[i] - 0.3455);
+                    double b = (y[i] + 1.7790);
+
+                    rgb[4 * i + 3] = 255;
+                    rgb[4 * i + 2] = (byte)Math.Round(r > 255 ? 255 : r < 0 ? 0 : r);
+                    rgb[4 * i + 1] = (byte)Math.Round(g > 255 ? 255 : g < 0 ? 0 : g);
+                    rgb[4 * i + 0] = (byte)Math.Round(b > 255 ? 255 : b < 0 ? 0 : b);
+                    Color c = Color.FromArgb(rgb[4 * i + 3], rgb[4 * i + 2], rgb[4 * i + 1], rgb[4 * i + 0]);
+                    bm.SetPixel(w, h, c);
+                }
+            }
+            bm.Save(folder+"\\" + isim + ".bmp");
+            comboBox2.Items.Add(isim);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bitmap oku = new Bitmap(folder + "\\" + comboBox2.SelectedItem + ".bmp");
+            pictureBox1.Image = Image.FromFile(folder + "\\" + comboBox2.SelectedItem + ".bmp");
+            //MessageBox.Show(folder + "\\" + comboBox2.SelectedItem + ".bmp");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            oynatilan = 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            try
+            {
+                pictureBox1.Image = Image.FromFile(folder + "\\" + oynatilan + ".bmp");
+            }
+            catch { }
+            
+            oynatilan++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            Random rand = new Random();
+            int sayi = rand.Next(0, 99999999);
+            folder = "D:\\YuvtoBmp_" + sayi;
+            Directory.CreateDirectory(folder);
+        }
+
+        public static byte[] ReadFile(Stream input)
+        {
+            byte[] buf = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buf, 0, buf.Length)) > 0)
+                {
+                    ms.Write(buf, 0, read);
+                }
+                return ms.ToArray();
+            }
+
+        }
+       
+    }
+}
